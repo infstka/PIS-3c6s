c3cadeba6411094e6fb8a1468c18a3da4ba8ade7
infstka <malder100@tut.by>
2023-05-25 19:57:57 +0300

    add LR6

---
+++ LR6/LR6/Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace LR6.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [ActionName("404")]
+        public ActionResult Error404()
+        {
+            return View();
+        }
+
+    }
+}
